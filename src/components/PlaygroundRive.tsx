7e1bf109c3afa733834ccb49f13b1e07e47e20ec
Sam Felton <sf.samfelton@icloud.com>
2025-05-28 19:50:16 +0100

    fixed height

--- src/components/PlaygroundRive.tsx
+++ src/components/PlaygroundRive.tsx
@@ -17,7 +17,7 @@ export default function PlaygroundRive() {
     <div
       onMouseEnter={() => hoveredInput && (hoveredInput.value = true)}
       onMouseLeave={() => hoveredInput && (hoveredInput.value = false)}
-      style={{ width: '100%', height: '400px', cursor: 'pointer' }}
+      style={{ width: '100%', height: '100px', cursor: 'pointer' }}
     >
       <RiveComponent />
     </div>
